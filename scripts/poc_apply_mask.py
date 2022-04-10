a595c8ce73b443427a3672b39b87ad19b34d908a
Will <yh2689@nyu.edu>
2022-04-10 19:26:42 +0800

    hi

--- scripts/poc_apply_mask.py
+++ scripts/poc_apply_mask.py
@@ -9,19 +9,17 @@ OUT_DIR = '../processed/masked'
 
 def mask_pics(in_dir, mask_dir, out_dir):
     img_ids = os.listdir(in_dir)
-    for img_id in img_ids:
+    for idx, img_id in enumerate(img_ids):
         img_path = f'{in_dir}/{img_id}'
         mask_path = f'{mask_dir}/{img_id[:-3]}png'
         
-        img = Image.open(img_path).convert('RGB')   
-        mask = Image.open(mask_path).convert('RGB')   
-        img = np.array(img)
-        mask = np.array(mask)
-
-        mask[mask > 0] = 1
-        img = img * mask
-        
-        cv2.imwrite(f'{OUT_DIR}/{img_id[:-4]}.png', img)
+        try:
+            image = cv2.imread(img_path) 
+            mask = cv2.imread(mask_path) 
+            masked = cv2.bitwise_and(image, mask)
+            cv2.imwrite(f'{OUT_DIR}/{img_id}', masked)
+        except:
+            continue
     return
 
 if __name__ == '__main__':
